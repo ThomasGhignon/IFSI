85ccf949c9adf21817ca284196f5903d00427b6a
ThomasGhignon <ghignon.thomas@gmail.com>
2020-01-25 13:22:17 +0100

    compteur ok

--- www/js/countdown.js
+++ www/js/countdown.js
@@ -1,48 +1,47 @@
 function countdownParam()
 {
   $(document).ready(function(){
-    duree="3600";
-    countdown();
+    var duree = localStorage.getItem("time");
+    countdown(duree);
   });
 }
 countdownParam();
 
-
-function countdown()
+function countdown(duree)
 {
-  var compteur = $('#timer');
-    s=duree;
-    m=0;h=0;
-    if(s<0)
+  localStorage.setItem("time", duree);
+  s=duree;
+  m=0;h=0;
+  if(s<0)
+  {
+    alert("fini"); //redirection fin de jeu (pas encore fait)
+  }
+  else
+  {
+    if(s>59)
     {
-      alert("fini");
-    }
-    else
-    {
-      if(s>59)
-      {
-        m=Math.floor(s/60);
-        s=s-m*60
-      }
-
-      if(m>59)
-      {
-        h=Math.floor(m/60);
-        m=m-h*60
-      }
-
-      if(s<10)
-      {
-        s="0"+s
-      }
-
-      if(m<10)
-      {
-        m="0"+m
-      }
+      m=Math.floor(s/60);
+      s=s-m*60
     }
 
-    duree=duree-1;
-    $('#timer').html("<span>"+m+":"+s+"</span>")
-    window.setTimeout("countdown();",1000);
+    if(m>59)
+    {
+      h=Math.floor(m/60);
+      m=m-h*60
+    }
+
+    if(s<10)
+    {
+      s="0"+s
+    }
+
+    if(m<10)
+    {
+      m="0"+m
+    }
+  }
+
+  duree=duree-1;
+  $('#timer').html("<span>"+m+":"+s+"</span>")
+  window.setTimeout("countdown("+duree+");",1000);
 }
--- www/js/start.js
+++ www/js/start.js
@@ -1,11 +1,9 @@
 function start_verif(root, path)
 {
-  console.log(root);
   $(document).ready(function(){
     $(".main").submit(function(event){
       event.preventDefault();
       var inputData = $("input").val();
-      console.log(inputData);
       if(inputData == "")
       {
         showError("Veuillez choisir un nom de service", "red");
@@ -13,6 +11,7 @@ function start_verif(root, path)
       }
       else
       {
+        localStorage.setItem("time","3600");
         //redirection
         $(location).attr('href',"http://"+root+"/IFSI/www/"+path+".php?valName="+inputData+"");
       }
